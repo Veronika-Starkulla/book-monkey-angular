d5b2a352b0d94e288399bc52d8ec1f035d31a474
Verostark <veronika.starkulla@gmail.com>
2022-02-06 16:58:08 +0100

    Kapitel 9 abgeschlossen

---
+++ src/app/book-details/book-details.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {Book} from "../shared/book";
+import {BookStoreService} from "../shared/book-store.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'bm-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.css']
+})
+export class BookDetailsComponent implements OnInit {
+
+  book?: Book;
+
+  constructor(private bs: BookStoreService, private route: ActivatedRoute) {
+  }
+  ngOnInit(): void {
+    const params = this.route.snapshot.paramMap;
+    this.book = this.bs.getSingle(params.get('isbn') || '');
+  }
+
+  getRating(num: number) {
+    return new Array(num);
+  }
+
+}
